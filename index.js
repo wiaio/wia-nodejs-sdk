b53b858a24cda450aa4d772fd44c1bb72927e603
Conall Laverty <conall.laverty@gmail.com>
2015-08-31 12:14:57 +0100

    Added UserClient constructor and REST methods

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var DeviceClient = require('./lib/DeviceClient');
+var UserClient = require('./lib/UserClient');
 
 module.exports = {
-    DeviceClient: DeviceClient
+    DeviceClient: DeviceClient,
+    UserClient: UserClient
 };
