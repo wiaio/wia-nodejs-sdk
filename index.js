179926bfd8551fa5d46673e48b46f61204b800c7
Conall Laverty <conall.laverty@gmail.com>
2015-11-24 10:20:57 +0000

    New SDK structure

--- index.js
+++
@@ -1,9 +0,0 @@
-  'use strict';
-
-var DeviceClient = require('./lib/DeviceClient');
-var UserClient = require('./lib/UserClient');
-
-module.exports = {
-    DeviceClient: DeviceClient,
-    UserClient: UserClient
-};
