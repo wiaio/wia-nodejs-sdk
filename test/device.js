bc21320804fe0c7c3b21b779c922855532237e92
Conall Laverty <conall.laverty@gmail.com>
2015-12-08 14:53:53 +0000

    Changed commands to functions

--- test/device.js
+++ test/device.js
@@ -9,49 +9,6 @@ describe('Device', function () {
         done();
     });
 
-    describe('#createDeviceClientAndConnect', function () {
-        it('should create a device client and connect to server', function (done) {
-            this.timeout(2500);
-            var DeviceClient = require('../lib/DeviceClient');
-
-            var client = new DeviceClient({token: deviceOptions.token});
-
-            client.on("connect", function() {
-                console.log("Client connected!");
-                done();
-            });
-
-            client.on("error", function(error) {
-                console.log("Error! - " + error);
-                done();
-            });
-        });
-    });
-
-    describe('#connectClientAndSendEvent', function () {
-        it('should connect a client and send an event', function (done) {
-            this.timeout(2500);
-            var DeviceClient = require('../lib/DeviceClient');
-
-            var client = new DeviceClient({token: deviceOptions.token});
-
-            client.on("connect", function() {
-                console.log("Client connected!");
-                client.publishEvent("Sensor", {
-                    temperature: 14.0
-                }, function() {
-                    console.log("In client callback!");
-                    done();
-                });
-            });
-
-            client.on("error", function(error) {
-                console.log("Error! - " + error);
-                done();
-            });
-        });
-    });
-
     after(function (done) {
         done();
     });
