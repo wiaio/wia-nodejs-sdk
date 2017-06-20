a856e1d0c030c2695fc1e5d847ae1c5de1ad345f
Conall Laverty <conall.laverty@gmail.com>
2017-06-20 16:11:26 +0100

    Fixes to coveralls integration

---
+++ test/device.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var expect = require('chai').expect;
+var testUtils = require('./testUtils');
+var wia = require('../wia')({
+  secretKey: testUtils.getUserSecretKey()
+});
+
+describe('Device', function () {
+  before(function (done) {
+      done();
+  });
+
+  describe('#createADevice', function () {
+    it('should create a device', function (done) {
+      var deviceName = "Device " + new Date().getTime();
+      wia.devices.create({
+        name: deviceName
+      }, function(error, device) {
+        expect(error).to.be.a.null;
+        expect(device).to.not.be.a.null;
+        expect(device.name).to.equal(deviceName);
+        expect(device.id).to.not.be.a.null;
+
+        done();
+      });
+    });
+  });
+
+  describe('#retrieveADevice', function () {
+    it('should retrieve a device', function (done) {
+      var deviceName = "Device " + new Date().getTime();
+      wia.devices.create({
+        name: deviceName
+      }, function(error, createdDevice) {
+        expect(error).to.be.a.null;
+        expect(createdDevice).to.not.be.a.null;
+        expect(createdDevice.name).to.equal(deviceName);
+        expect(createdDevice.id).to.not.be.a.null;
+
+        wia.devices.retrieve(createdDevice.id, function(error, retrievedDevice) {
+          expect(error).to.be.a.null;
+          expect(retrievedDevice).to.not.be.a.null;
+          expect(createdDevice.id).to.equal(retrievedDevice.id);
+
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#updateADevice', function () {
+    it('should update a device', function (done) {
+      var deviceName = "Device " + new Date().getTime();
+      wia.devices.create({
+        name: deviceName
+      }, function(error, createdDevice) {
+        expect(error).to.be.a.null;
+        expect(createdDevice).to.not.be.a.null;
+
+        var newDeviceName = "Device " + new Date().getTime();
+        wia.devices.update(createdDevice.id, {
+            name: newDeviceName
+          }, function(error, updatedDevice) {
+            expect(error).to.be.a.null;
+            expect(updatedDevice).to.not.be.a.null;
+            expect(updatedDevice.name).to.equal(newDeviceName);
+
+            done();
+          });
+      });
+    });
+  });
+
+  describe('#deleteADevice', function () {
+    it('should delete a device', function (done) {
+      var deviceName = "Device " + new Date().getTime();
+      wia.devices.create({
+        name: deviceName
+      }, function(error, device) {
+        expect(error).to.be.a.null;
+        expect(device).to.not.be.a.null;
+
+        wia.devices.delete(device.id, function(error, deleted) {
+            expect(error).to.be.a.null;
+            expect(deleted).to.not.be.a.null;
+            expect(deleted).to.be.true;
+
+            done();
+          });
+      });
+    });
+  });
+
+  describe('#listDevices', function () {
+    it('should list devices', function (done) {
+      var deviceName = "Device " + new Date().getTime();
+      wia.devices.list({
+        limit: 20,
+        page: 0
+      }, function(error, data) {
+        expect(error).to.be.a.null;
+        expect(data).to.not.be.a.null;
+        expect(data.devices).to.not.be.a.null;
+        expect(data.devices).to.be.instanceof(Array);
+        expect(data.count).to.not.be.a.null;
+        expect(data.count).to.be.a('number');
+        done();
+      });
+    });
+  });
+
+  after(function (done) {
+      done();
+  });
+});
---
+++ test/stream.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var expect = require('chai').expect;
+var testUtils = require('./testUtils');
+var wia = require('../wia')({
+  secretKey: testUtils.getUserSecretKey(),
+  stream: testUtils.getStreamConfig()
+});
+
+describe('Stream', function () {
+  before(function (done) {
+    done();
+  });
+
+  beforeEach(function(done) {
+    wia.stream.disconnect();
+    setTimeout(function() {
+      done();
+    }, 150);
+  });
+
+  describe('#connectToStream', function () {
+    it('should connect to the stream', function (done) {
+      var doneCalled = false;
+      wia.stream.on("connect", function() {
+        console.log("Stream got connected!");
+        if (!doneCalled) {
+          doneCalled = true;
+          done();
+        }
+      });
+      wia.stream.connect();
+    });
+  });
+
+  describe('#connectAndDisconnectToStream', function () {
+    it('should connect and disconnect to the stream', function (done) {
+      var disconnectCalled = false;
+
+      wia.stream.on("connect", function() {
+        console.log("Stream got connected!");
+        if (!disconnectCalled) {
+          console.log("Calling disconnect.");
+          disconnectCalled = true;
+          wia.stream.disconnect();
+        }
+      });
+
+      wia.stream.on("disconnect", function() {
+        console.log("Stream got disconnected!");
+        done();
+      });
+
+      wia.stream.connect();
+    });
+  });
+
+  after(function (done) {
+      done();
+  });
+});
